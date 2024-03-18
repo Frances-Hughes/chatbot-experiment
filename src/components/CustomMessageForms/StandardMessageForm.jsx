5c0106092b8550717ce94272ed4297ff21742b36
Frances Hughes <franceslhughes@hotmail.com>
2024-03-18 15:04:21 +1300

    When attaching images and sending messages, this is displayed in the chat
    When attaching images and sending messages, this is displayed in the chat

--- src/components/CustomMessageForms/StandardMessageForm.jsx
+++ src/components/CustomMessageForms/StandardMessageForm.jsx
@@ -12,7 +12,25 @@ const StandardMessageForm = ({ props, activeChat }) => {
   const [preview, setPreview] = useState("");
 
   const handleChange = (e) => setMessage(e.target.value);
-  const handleSubmit = async () => {};
+
+  const handleSubmit = async () => {
+    const date = new Date()
+      .toISOString()
+      .replace("T", " ")
+      .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+    const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
+
+    const form = {
+      attachments: at,
+      created: date,
+      sender_username: props.username,
+      text: message,
+      activeChatId: activeChat.id,
+    };
+    props.onSubmit(form);
+    setMessage("");
+    setAttachment("");
+  };
 
   return (
     <div className="message-form-container">
@@ -68,6 +86,7 @@ const StandardMessageForm = ({ props, activeChat }) => {
             className="icon-airplane"
             onClick={() => {
               setPreview("");
+              handleSubmit();
             }}
           />
         </div>
